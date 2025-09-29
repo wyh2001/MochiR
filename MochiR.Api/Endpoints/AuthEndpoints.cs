0ec33d6ad428d5e30ccb2888a00ff8b1e80aa3c2
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-09-29 03:46:49 -0400

    Update `/register` and `/login` endpoint

--- MochiR.Api/Endpoints/AuthEndpoints.cs
+++ MochiR.Api/Endpoints/AuthEndpoints.cs
@@ -9,14 +9,47 @@ namespace MochiR.Api.Endpoints
         {
             var group = routes.MapGroup("/api/auth").WithTags("Auth");
 
-            group.MapPost("/register", (RegisterDto registerDto, UserManager<ApplicationUser> userManager) =>
+            group.MapPost("/register", async (
+                RegisterDto registerDto, 
+                UserManager<ApplicationUser> userManager) =>
             {
-                return Results.Ok("Register endpoint is working...");
+                var user = new ApplicationUser
+                {
+                    UserName = registerDto.UserName,
+                    Email = registerDto.Email,
+                    CreatedAtUtc = DateTime.UtcNow
+                };
+                var result = await userManager.CreateAsync(user, registerDto.Password);
+                return result.Succeeded ? Results.Ok() : Results.BadRequest(result.Errors);
             });
 
-            group.MapPost("/login", (LoginDto loginDto, SignInManager<ApplicationUser> signInManager) =>
+            group.MapPost("/login", async (
+                LoginDto loginDto, 
+                SignInManager<ApplicationUser> signInManager, 
+                UserManager<ApplicationUser> userManager) =>
             {
-                return Results.Ok("Login endpoint is working...");
+                var userNameOrEmail = loginDto.UserNameOrEmail.Trim();
+                var password = loginDto.Password;
+                ApplicationUser? user = await userManager.FindByNameAsync(userNameOrEmail) ?? await userManager.FindByEmailAsync(userNameOrEmail);
+                var userNameForSignIn = user?.UserName ?? userNameOrEmail;
+
+                // No matter the user exists or not, proceed to avoid account enumeration
+                var result = await signInManager.PasswordSignInAsync(
+                    userNameForSignIn,
+                    password,
+                    isPersistent: false,
+                    lockoutOnFailure: true);
+
+                if (result.Succeeded)
+                    return Results.Ok();
+                if (result.IsLockedOut)
+                    return Results.StatusCode(423);
+                if (result.RequiresTwoFactor)
+                    return Results.StatusCode(403); // TBD: handle 2FA
+                if (result.IsNotAllowed)
+                    return Results.Forbid();
+
+                return Results.BadRequest();
             });
 
             group.MapPost("/logout", async (SignInManager<ApplicationUser> signInManager) =>
