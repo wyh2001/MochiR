349dedf797960899c4c525f972681f1f368973f8
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-11-06 16:58:36 -0500

    Migrate (add FollowersCount and FollowingCount to AspNetUsers table)

---
+++ MochiR.Api/Infrastructure/Migrations/20251106215730_UpdateUserWithFollowCounts.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MochiR.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserWithFollowCounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FollowersCount",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FollowingCount",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FollowersCount",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FollowingCount",
+                table: "AspNetUsers");
+        }
+    }
+}
