fd6d958b9f540846665c65a457b33cd0b98e5a1a
Yihang Wang <31209506+wyh2001@users.noreply.github.com>
2025-11-16 21:54:45 -0500

    Migrate removing unaccent

---
+++ MochiR.Api/Infrastructure/Migrations/20251117025311_RemoveUnaccent.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace MochiR.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnaccent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                table: "Subjects",
+                type: "tsvector",
+                nullable: true,
+                computedColumnSql: "setweight(to_tsvector('simple', coalesce(\"Name\", '')), 'A') || setweight(to_tsvector('simple', coalesce(\"Slug\", '')), 'B')",
+                stored: true,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector",
+                oldNullable: true,
+                oldComputedColumnSql: "setweight(to_tsvector('simple', unaccent(coalesce(\"Name\", ''))), 'A') || setweight(to_tsvector('simple', unaccent(coalesce(\"Slug\", ''))), 'B')",
+                oldStored: true);
+
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                table: "Reviews",
+                type: "tsvector",
+                nullable: true,
+                computedColumnSql: "setweight(to_tsvector('simple', coalesce(\"Title\", '')), 'A') || setweight(to_tsvector('simple', coalesce(\"Excerpt\", '')), 'B') || setweight(to_tsvector('simple', coalesce(\"Content\", '')), 'C')",
+                stored: true,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector",
+                oldNullable: true,
+                oldComputedColumnSql: "setweight(to_tsvector('simple', unaccent(coalesce(\"Title\", ''))), 'A') || setweight(to_tsvector('simple', unaccent(coalesce(\"Excerpt\", ''))), 'B') || setweight(to_tsvector('simple', unaccent(coalesce(\"Content\", ''))), 'C')",
+                oldStored: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                table: "Subjects",
+                type: "tsvector",
+                nullable: true,
+                computedColumnSql: "setweight(to_tsvector('simple', unaccent(coalesce(\"Name\", ''))), 'A') || setweight(to_tsvector('simple', unaccent(coalesce(\"Slug\", ''))), 'B')",
+                stored: true,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector",
+                oldNullable: true,
+                oldComputedColumnSql: "setweight(to_tsvector('simple', coalesce(\"Name\", '')), 'A') || setweight(to_tsvector('simple', coalesce(\"Slug\", '')), 'B')",
+                oldStored: true);
+
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "SearchVector",
+                table: "Reviews",
+                type: "tsvector",
+                nullable: true,
+                computedColumnSql: "setweight(to_tsvector('simple', unaccent(coalesce(\"Title\", ''))), 'A') || setweight(to_tsvector('simple', unaccent(coalesce(\"Excerpt\", ''))), 'B') || setweight(to_tsvector('simple', unaccent(coalesce(\"Content\", ''))), 'C')",
+                stored: true,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector",
+                oldNullable: true,
+                oldComputedColumnSql: "setweight(to_tsvector('simple', coalesce(\"Title\", '')), 'A') || setweight(to_tsvector('simple', coalesce(\"Excerpt\", '')), 'B') || setweight(to_tsvector('simple', coalesce(\"Content\", '')), 'C')",
+                oldStored: true);
+        }
+    }
+}
